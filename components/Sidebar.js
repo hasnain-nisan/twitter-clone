532c2d5a0aa5fec77f3f91a5921d90ada60a9322
hasnain-nisan <68366713+hasnain-nisan@users.noreply.github.com>
2022-01-17 16:29:52 +0600

    Image host name resolved

---
+++ components/Sidebar.js
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+import React from 'react'
+
+const Sidebar = () => {
+    return (
+      <div className="hidden sm:flex flex-col items-center xl:items-start xl:w-[340px] p-2 fixed h-full">
+        <div className="flex items-center justify-center w-14 h-14 p-0 xl:ml-24 hoverAnimation">
+            <Image src="https://rb.gy/ogau5a" />
+        </div>
+        <p className="text-white">Sidebar</p>
+      </div>
+    );
+}
+
+export default Sidebar
