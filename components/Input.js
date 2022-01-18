255ff852eb9881c213e1bc78c59cdbc168b3d3c3
hasnain-nisan <68366713+hasnain-nisan@users.noreply.github.com>
2022-01-18 17:14:07 +0600

    Input component done

--- components/Input.js
+++ components/Input.js
@@ -1,6 +1,15 @@
 import React, {useState, useRef} from 'react'
 import "emoji-mart/css/emoji-mart.css";
 import { Picker } from "emoji-mart";
+import {db, storage} from '../firbase'
+import { getDownloadURL, ref, uploadString } from 'firebase/storage';
+import {
+    addDoc,
+    collection,
+    doc,
+    serverTimestamp,
+    updateDoc
+} from '@firebase/firestore'
 import {
   MdCancel,
   MdImage,
@@ -18,14 +27,46 @@ const Input = () => {
 
     const filePickerRef = useRef()
 
-    const sendPost = () => {
+    const sendPost = async () => {
         if(loading) return;
         setLoading(true)
 
-        // const docRef
+        const docRef = await addDoc(collection(db, 'posts'), {
+            // id: session.user.uid,
+            // username: session.user.name,
+            // userImg: session.user.image,
+            // tag: session.user.tag,
+            text: input,
+            timestamp: serverTimestamp(),
+        })
+
+        const imageRef = ref(storage, `posts/${docRef.id}/image`)
+
+        if(selectedFile) {
+            await uploadString(imageRef, selectedFile, "data_url")
+                .then(async () => {
+                    const downloadURL = await getDownloadURL(imageRef);
+                    await updateDoc(doc(db, 'posts', docRef.id), {
+                        image: downloadURL
+                    })
+                })
+        }
+
+        setLoading(false)
+        setInput("")
+        setSelectedFile(null)
+        setShowEmojis(false)
     };
 
-    const addImageToPost = () => {
+    const addImageToPost = (e) => {
+        const reader = new FileReader()
+        if(e.target.files[0]){
+            reader.readAsDataURL(e.target.files[0])
+        }
+
+        reader.onload = (readerEvent) => {
+            setSelectedFile(readerEvent.target.result)
+        }
     }
 
     const addEmoji = (e) => {
@@ -38,7 +79,7 @@ const Input = () => {
 
     return (
       <div
-        className={`border-b border-gray-700 p-3 flex items-start space-x-3`}
+        className={`border-b border-gray-700 p-3 flex items-start space-x-3 ${loading && "opacity-60"}`}
       >
         <img
           src="https://source.unsplash.com/random/1600*900/?profile"
@@ -46,7 +87,9 @@ const Input = () => {
           className="h-11 w-11 rounded-full cursor-pointer"
         />
         <div className="w-full divide-y divide-gray-700">
-          <div className={`${selectedFile && 'pb-7'} ${input && 'space-y-2.5'}`}>
+          <div
+            className={`${selectedFile && "pb-7"} ${input && "space-y-2.5"}`}
+          >
             <textarea
               value={input}
               onChange={(e) => setInput(e.target.value)}
@@ -72,57 +115,58 @@ const Input = () => {
             )}
           </div>
 
-          <div className="flex items-center justify-between pt-2.5">
-            <div className="flex items-center">
-              <div
-                className="icon"
-                onClick={() => filePickerRef.current.click()}
-              >
-                <MdImage className="h-[22px] text-[#1d9bf0]" />
-                <input
-                  type="file"
-                  className="hidden"
-                  ref={filePickerRef}
-                  onChange={addImageToPost}
-                />
-              </div>
-              <div className="icon rotate-90">
-                <MdBarChart className="h-[22px] text-[#1d9bf0]" />
-              </div>
-              <div
-                className="icon"
-                onClick={() => setShowEmojis(!showEmojis)}
-              >
-                <MdInsertEmoticon className="h-[22px] text-[#1d9bf0]" />
-              </div>
-              <div className="icon">
-                <MdDateRange className="h-[22px] text-[#1d9bf0]" />
-              </div>
+          {!loading && (
+            <div className="flex items-center justify-between pt-2.5">
+              <div className="flex items-center">
+                <div
+                  className="icon"
+                  onClick={() => filePickerRef.current.click()}
+                >
+                  <MdImage className="h-[22px] text-[#1d9bf0]" />
+                  <input
+                    type="file"
+                    className="hidden"
+                    ref={filePickerRef}
+                    onChange={addImageToPost}
+                  />
+                </div>
+                <div className="icon rotate-90">
+                  <MdBarChart className="h-[22px] text-[#1d9bf0]" />
+                </div>
+                <div
+                  className="icon"
+                  onClick={() => setShowEmojis(!showEmojis)}
+                >
+                  <MdInsertEmoticon className="h-[22px] text-[#1d9bf0]" />
+                </div>
+                <div className="icon">
+                  <MdDateRange className="h-[22px] text-[#1d9bf0]" />
+                </div>
 
-              {showEmojis && (
-                  <Picker 
+                {showEmojis && (
+                  <Picker
                     onSelect={addEmoji}
                     theme="dark"
-                    style={{ 
-                        position: 'absolute',
-                        marginTop: "465px",
-                        marginLeft: "-40",
-                        maxWidth: "320px",
-                        borderRadius: '20px',
-                        borderColor: 'gray'
-                     }}
+                    style={{
+                      position: "absolute",
+                      marginTop: "465px",
+                      marginLeft: "-40",
+                      maxWidth: "320px",
+                      borderRadius: "20px",
+                      borderColor: "gray",
+                    }}
                   />
-              )}
-            </div>
-            <button 
+                )}
+              </div>
+              <button
                 disabled={!input.trim() && !selectedFile ? true : false}
                 className="bg-[#1d9bf0] text-white rounded-full px-4 py-1.5 font-mold shadow-md hover:bg-[#1a8cd8] disabled:hover:bg-[#1d9bf0] disabled:opacity-50 disabled:cursor-not-allowed"
                 onClick={sendPost}
-            >
+              >
                 Tweet
-            </button>
-          </div>
-
+              </button>
+            </div>
+          )}
         </div>
       </div>
     );
