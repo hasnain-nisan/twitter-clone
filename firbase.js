7ceeeab6638fca2645f9dc2a3396da1652dcddd8
hasnain-nisan <68366713+hasnain-nisan@users.noreply.github.com>
2022-01-18 15:28:52 +0600

    Firbase.js setup

--- firbase.js
+++ firbase.js
@@ -1,3 +1,12 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyA4m6ELzre5oeJBjeKJUt2SbsuO_PGkdyw",
   authDomain: "twitter-clone-react-554b3.firebaseapp.com",
@@ -6,3 +15,11 @@ const firebaseConfig = {
   messagingSenderId: "45382816799",
   appId: "1:45382816799:web:a23a9d1f0498e02adff12a",
 };
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore();
+const storage = getStorage();
+
+export default app;
+export { db, storage };
