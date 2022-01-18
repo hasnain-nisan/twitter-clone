f4bc4a6608a1f3826a2001fb739a4f49c0359e68
hasnain-nisan <68366713+hasnain-nisan@users.noreply.github.com>
2022-01-18 17:52:34 +0600

    initializing next auth

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import Sidebar from '../components/Sidebar';
 import Feed from '../components/Feed'
 import styles from '../styles/Home.module.css'
+import { getProviders, getSession, useSession } from "next-auth/react";
 
 export default function Home() {
   return (
@@ -25,4 +26,26 @@ export default function Home() {
       </main>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const trendingResults = await fetch("https://jsonkeeper.com/b/NKEV").then(
+    (res) => res.json()
+  )
+  const followResults = await fetch("https://jsonkeeper.com/b/WWMJ").then(
+    (res) => res.json()
+  )
+
+  const providers = await getProviders()
+  const session = await getSession(context)
+
+  return {
+    props: {
+      trendingResults,
+      followResults,
+      providers,
+      session
+    }
+  }
+  
 }
