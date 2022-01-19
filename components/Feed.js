c311957548015871283a33320f8e6b2f3c3b8afe
hasnain-nisan <68366713+hasnain-nisan@users.noreply.github.com>
2022-01-19 15:17:29 +0600

    Post component

--- components/Feed.js
+++ components/Feed.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {HiSparkles} from 'react-icons/hi'
+import { onSnapshot, collection, query, orderBy } from "@firebase/firestore";
+import {db} from "../firbase"
 import Input from './Input'
+import Post from './Post'
+
 
 const Feed = () => {
+
+    const [posts, setPosts] = useState([])
+
+    useEffect(
+      () =>
+        onSnapshot(
+          query(collection(db, "posts"), orderBy("timestamp", "desc")),
+          (snapshot) => {
+            setPosts(snapshot.docs);
+          }
+        ),
+      [db]
+    );
+
     return (
         <div className="text-white flex-grow border-l border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[370px]">
             <div className="text-[#d9d9d9] flex items-center sm:justify-between py-2 px-3 sticky top-0 z-50 bg-black border-b border-gray-700">
@@ -13,6 +31,14 @@ const Feed = () => {
             </div>
 
             <Input/>
+
+            <div className="pb-72">
+                {posts.length > 0 ? (
+                    posts.map(post => <Post key={post.id} id={post.id} post={post.data()}/>)
+                ) : (
+                    "no post available"
+                )}
+            </div>
         </div>
     )
 }
